60d2fd6fdc36be2a0ef728044ca2e48aa183894c
shahbaz-arbisoft <shahbaz.shabbir@arbisoft.com>
2022-08-04 03:58:07 +0500

    Heroku deployment

--- blood_donation_platform/settings.py
+++ blood_donation_platform/settings.py
@@ -189,3 +189,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
+
+
+import django_heroku
+django_heroku.settings(locals())
